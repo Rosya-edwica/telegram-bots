1039661c6d0b4fae46025b4fa1d964a6c2e4131b
Rosya-edwica <rosya-8@yandex.ru>
2023-03-21 16:28:15 +0300

    добавил бота, который ищет дубликаты

---
+++ duplicate-search/bot.py
@@ -0,0 +1,156 @@
+# coding=utf-8
+import asyncio
+from contextlib import suppress
+
+
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext   
+from aiogram.utils.exceptions import MessageCantBeDeleted, MessageToDeleteNotFound
+
+import config
+import database
+import os
+
+from loguru import logger
+
+
+bot = Bot(config.TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+# Она появилась потому, что без этого бот будет считать предыдущее сообщение пользователя ответом на текущий вопрос (ответа на который ожидает бот)
+# И когда пользователь введет да/нет, это автоматически запишется в ответ того вопроса, который должен появиться после этого ответа
+# Эта переменная необходима для того, чтобы контролировать значения в состояниях ответов пользователя
+CURRENT_QUESTION_ID = None 
+
+os.makedirs('logs', exist_ok=True)
+logger.remove() # Запрещаем выводить сообщения в терминал
+logger.add("logs/info.log", format="{time} {level} {message}", level="INFO", rotation="50 MB", compression="zip", mode="w")
+
+# Начало
+@dp.message_handler(commands='start')
+async def run_bot(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Готов')
+    
+    # Запускаем вспомогательный метод, с которого всё начнется 
+    await config.StateMachine.start_question.set()  # Показываем следующий вопрос
+    await message.answer(config.WELCOME_TEXT)
+    logger.info(f"[{message.from_user.username}] - Начал работу с ботом")
+    await message.answer('Для начала напишите мне - готов', reply_markup=markup)
+
+
+@dp.message_handler(state=config.StateMachine.start_question)
+async def start(message: types.Message, state: FSMContext):
+    """Вспомогательный метод для запуска первого вопроса. Без этого метода, дублируются первые два вопросы и сбивается весь порядок ответов
+    Поэтому был написан этот метод, как отправная точка и добавлена переменная CURRENT_QUESTION_ID"""
+
+    global CURRENT_QUESTION_ID
+    if message.text.lower() != "готов":
+        logger.info(f"[{message.from_user.username}] - Неправильный ответ")
+        asyncio.create_task(input_invalid(message))
+        return
+
+    couple_skills = database.get_couple_skills_from_database()
+    if not couple_skills:
+        logger.warning(f"[{message.from_user.username}] - Вопросы закончились!")
+        await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
+        await state.finish()
+    logger.info(f"[{message.from_user.username}] - Пара: {couple_skills.demand_name}&{couple_skills.dup_demand_name}")
+    CURRENT_QUESTION_ID = couple_skills.id # Меняем значение нашей переменной, тем самым указывая корректный айди вопроса, который нужно обработать
+
+    all_values, remains = database.get_how_much_is_left()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Нет', 'Да')
+    await message.answer("\n".join((
+                "Одинаковые навыки?",
+                f"1️⃣ \t{couple_skills.demand_name.capitalize()}",
+                f"1️⃣ \t{couple_skills.dup_demand_name.capitalize()}",
+                f"🤔Совместимость: {couple_skills.similarity}%",
+                f"Осталось:{remains}:{all_values}"
+                )), reply_markup=markup)
+    await config.StateMachine.question.set()
+
+
+# Переключение между вопросами 
+@dp.message_handler(state=config.StateMachine.question)
+async def show_question(message: types.Message, state: FSMContext):
+    """
+    Основной метод, который будет обрабатывать ответы пользователя и изменять значения в БД
+    """
+    global CURRENT_QUESTION_ID
+    if message.text.lower() not in {"да", "назад", "нет"}:
+        asyncio.create_task(input_invalid(message))
+        return
+
+    # Меняем значения в БД
+    if message.text.lower().strip() == "назад":
+        logger.debug(f"[{message.from_user.username}] Back")
+        previos_skill = database.get_last_viewed_skill()
+        all_values, remains = database.get_how_much_is_left()
+        if previos_skill:
+            await config.StateMachine.question.set()
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+            markup.add('Нет', 'Назад', 'Да')
+            await message.answer("\n".join((
+                "Одинаковые навыки?",
+                f"1️⃣ \t{previos_skill.demand_name.capitalize()}",
+                f"1️⃣ \t{previos_skill.dup_demand_name.capitalize()}",
+                f"🤔Совместимость: {previos_skill.similarity}%",
+                f"Осталось:{remains}:{all_values}"
+                )), reply_markup=markup)
+            logger.info(f"[{message.from_user.username}] - Пара: {previos_skill.demand_name}&{previos_skill.dup_demand_name}")
+            CURRENT_QUESTION_ID = previos_skill.id
+        else:
+            await message.answer("Ранее вы еще не отвечали на вопросы. Ответьте на предыдущий вопрос")
+        
+        
+    else:
+        if message.text.lower().strip() == 'да':
+            database.confirm_similarity(couple_id=CURRENT_QUESTION_ID)
+            logger.warning(f"[{message.from_user.username}] - True: {CURRENT_QUESTION_ID}")
+
+        elif message.text.lower().strip() == 'нет':
+            logger.warning(f"[{message.from_user.username}] - False: {CURRENT_QUESTION_ID}")
+            database.confirm_similarity(couple_id=CURRENT_QUESTION_ID, confirm=False)
+        
+        # Получаем новый вопрос
+        couple_skills = database.get_couple_skills_from_database()
+        if not couple_skills:     
+            logger.warning(f"[{message.from_user.username}] - Вопросы закончились!")
+            await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
+            await state.finish()
+        logger.info(f"[{message.from_user.username}] - Пара: {couple_skills.demand_name}&{couple_skills.dup_demand_name}")
+        CURRENT_QUESTION_ID = couple_skills.id
+        
+        # Показываем вопрос пользователю
+        all_values, remains = database.get_how_much_is_left()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+        markup.add('Нет', 'Назад', 'Да')
+        await config.StateMachine.question.set() # Показываем следующий вопрос
+        await message.answer("\n".join((
+                "Одинаковые навыки?",
+                f"1️⃣ \t{couple_skills.demand_name.capitalize()}",
+                f"1️⃣ \t{couple_skills.dup_demand_name.capitalize()}",
+                f"🤔Совместимость: {couple_skills.similarity}%",
+                f"Осталось:{remains}:{all_values}"
+                )), reply_markup=markup)
+
+
+# Функция для удаления сообщений
+async def delete_message(message: types.Message, sleep_time: int = 0):
+    await asyncio.sleep(sleep_time)
+    with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
+        await message.delete()
+
+
+# Обработка исключений
+@dp.message_handler(state=config.StateMachine.question)
+async def input_invalid(message: types.Message):
+    await config.StateMachine.question.set()
+    return await message.reply("Неправильный ответ. Используйте клавиатуру или введите ответ самостоятельно")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ duplicate-search/config.py
@@ -0,0 +1,46 @@
+"""Модуль, содержащий объявление основных объектов бота"""
+import os
+import logging
+from dataclasses import dataclass
+from enum import Enum
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+WELCOME_TEXT = """Вам будет представлен список пар навыков, схожих по некоторому принципу. Вам нужно подвердить/опровергнуть сходство навыков.
+        Какие навыки ЯВЛЯЮТСЯ дубликатами?
+        1. Навык состоит из нескольких слов и отличается от другого навыка только порядком этих слов
+        2. Навык похож по смыслу, но отличается по написанию
+        Какие навыки НЕ ЯВЛЯЮТСЯ дубликатами?
+        1. Если написание навыка индетично другому навыку, но у них отличается номер версии, то такие навыки НЕ ДУБЛИКАТЫ 
+        Например: Python3 и Python2 - Два разных навыка
+        Внимание: Python3.7 и Python3.10 - это уже одинаковые навыки, т.к первая целая часть версии, у них одинаковая
+        """
+
+TOKEN = os.getenv("TOKEN_DUPLICATE_SEARCH")
+HOST = os.getenv("EDWICA_DB_HOST")
+USER = os.getenv("EDWICA_DB_USER")
+PASS = os.getenv("EDWICA_DB_PASS")
+
+class MYSQL(Enum):
+    HOST = HOST
+    USER = USER
+    PASSWORD = PASS
+    PORT = 3306
+    DB = "edwica"
+    TABLE = "demand_duplicate"
+
+@dataclass
+class SimilarCouple:
+    id: int
+    demand_id : int
+    demand_name: str
+    dup_demand_id: int
+    dup_demand_name: str
+    similarity: int
+    is_duplicate: bool
+
+
+class StateMachine(StatesGroup):
+    start_question = State()
+    question = State()
+    last = State()
