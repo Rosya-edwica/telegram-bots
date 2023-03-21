681f537f6f40b913419e70fa2ee3f63c5f64c72d
Rosya-edwica <rosya-8@yandex.ru>
2023-03-21 19:54:37 +0300

    заменил название токена

--- leave-or-not/config.py
+++ leave-or-not/config.py
@@ -5,7 +5,7 @@ from typing import NamedTuple
 from enum import Enum
 
 
-TOKEN = os.getenv("TOKEN_SKILLS_LIKE_PROFESSIONS")
+TOKEN = os.getenv("TOKEN_LEAVE_OR_NOT")
 HOST = os.getenv("EDWICA_DB_HOST")
 USER = os.getenv("EDWICA_DB_USER")
 PASS = os.getenv("EDWICA_DB_PASS")
