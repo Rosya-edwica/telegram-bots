f6f8b564c81e2eecf4b1dc35506525e0aa68b1ae
Rosya-edwica <rosya-8@yandex.ru>
2023-03-21 19:53:16 +0300

    подобрал более подходящее название для бота

---
+++ leave-or-not/database.py
@@ -0,0 +1,91 @@
+import pymysql
+from pymysql.connections import Connection
+
+from config import Skill, MYSQL
+
+def connect_to_db() -> Connection:
+    try:
+        connection = pymysql.connect(
+                host=MYSQL.HOST.value,
+                port=MYSQL.PORT.value,
+                database=MYSQL.DB.value,
+                user=MYSQL.USER.value,
+                password=MYSQL.PASSWORD.value,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        return connection
+
+    except Exception as ex:
+        exit(f"Error by connection: {ex}")
+
+
+def get_skill_from_database() -> Skill:
+    """Возвращает самую первую пару навыков, у которой значение is_duplicate является None. 
+    Таким образом, мы понимаем, что с этой парой еще не работали"""
+    
+    connection = connect_to_db()
+    with connection.cursor() as cursor:
+        query_get_null_couple = f"SELECT * FROM {MYSQL.TABLE.value}  WHERE is_displayed IS NULL LIMIT 1"
+
+        cursor.execute(query_get_null_couple)
+        skill = cursor.fetchone()
+        connection.close()
+        if skill:return Skill(iD=skill['id'], title=skill['name'], is_dislayed=skill['is_displayed'])
+        else:return
+
+def confirm_skill(id: int, confirm: bool = True) -> None:
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            if confirm:
+                cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=1 WHERE id={id}")
+            else:
+                cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=0 WHERE id={id}")
+
+            connection.commit()
+    finally:
+        connection.close()
+
+
+def refute_skill(id: int) -> None: # Опровергнуть сходство
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=null WHERE id={id}")
+            connection.commit()
+
+    finally:
+        connection.close()
+
+def get_previos_skill(current_id: int) -> Skill | None:
+    connection = connect_to_db()
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT * FROM {MYSQL.TABLE.value} WHERE id = {current_id-1}""")
+        skill = cursor.fetchone()
+
+        try:res = Skill(iD=skill['id'], title=skill['name'], is_dislayed=skill['is_displayed'])
+        except: res = None 
+
+        if res is None: return
+        refute_skill(id=res.iD) # Делаем это для того, чтобы мы могли несколько раз подряд нажимать кнопку назад 
+        connection.close()
+        return res
+
+def show_the_rest() -> tuple[int, int]:
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value}")
+            count_all_values = cursor.fetchone()["COUNT(*)"]
+
+            cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value} WHERE is_displayed IS NULL")
+            count_null_values = cursor.fetchone()["COUNT(*)"]
+            return count_all_values, count_null_values
+    except BaseException as err:
+        exit(err)
+    finally:
+        connection.close()
+
+
+if __name__ == "__main__":
+    print(get_skill_from_database())
---
+++ leave-or-not/main.py
@@ -0,0 +1,122 @@
+from aiogram import Bot, executor, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.exceptions import MessageCantBeDeleted, MessageToDeleteNotFound
+from aiogram.dispatcher import FSMContext
+import asyncio
+from loguru import logger
+
+import os
+from contextlib import suppress
+
+import database
+from config import States, TOKEN
+
+
+CURRENT_SKILL_ID = None
+
+bot = Bot(TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+os.makedirs("logs/", exist_ok=True)
+logger.remove()
+logger.add("logs/info.log", format="{time} {level} {message}", level="INFO", rotation="10 MB", compression="zip", mode="w")
+
+@dp.message_handler(commands='start')
+async def run_bot(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Готов')
+    
+    # Запускаем вспомогательный метод, с которого всё начнется 
+    await States.start_question.set()  # Показываем следующий вопрос
+    await message.answer('Для начала напишите мне - готов', reply_markup=markup)
+    logger.info("%s Начал работу с ботом", message.from_user.username)
+
+
+@dp.message_handler(state=States.start_question)
+async def start(message: types.Message):
+    """Вспомогательный метод для запуска первого вопроса. Без этого метода, дублируются первые два вопросы и сбивается весь порядок ответов
+    Поэтому был написан этот метод, как отправная точка и добавлена переменная CURRENT_QUESTION_ID"""
+
+    global CURRENT_SKILL_ID
+    if message.text.lower() != "готов":
+        asyncio.create_task(input_invalid(message))
+        logger.info("Неправильный ответ на вопрос '%s'", message.text)
+        return
+
+    skill = database.get_skill_from_database()
+    if not skill:
+        logger.warning("Закончились вопросы")
+        await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
+        quit()
+    CURRENT_SKILL_ID = skill.iD # Меняем значение нашей переменной, тем самым указывая корректный айди вопроса, который нужно обработать
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Нет', 'Да')
+    _, remains = database.show_the_rest()
+    await message.answer(f"Оставить навык?\n- {skill.title}\nОсталось:{remains}", reply_markup=markup)
+    await States.question.set()
+
+# Переключение между вопросами 
+@dp.message_handler(state=States.question)
+async def show_question(message: types.Message, state: FSMContext):
+    """
+    Основной метод, который будет обрабатывать ответы пользователя и изменять значения в БД
+    """
+    global CURRENT_SKILL_ID
+    if message.text.lower() not in {"да", "назад", "нет"}:
+        asyncio.create_task(input_invalid(message))
+        return
+    
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Нет', 'Назад', 'Да')
+    if message.text.lower().strip() == 'назад':
+        previos_skill = database.get_previos_skill(CURRENT_SKILL_ID)
+        if previos_skill:
+            await States.question.set()
+            _, remains = database.show_the_rest()
+            await message.answer(f"Оставить навык?\n- {previos_skill.title}\nОсталось:{remains}", reply_markup=markup)
+            CURRENT_SKILL_ID = previos_skill.iD
+
+        else:
+            await message.answer("Ранее вы еще не отвечали на вопросы!")
+    else:
+        # Меняем значения в БД
+        if message.text.lower().strip() == 'да':
+            database.confirm_skill(id=CURRENT_SKILL_ID)
+            logger.info("Прошел проверку навык с id: %d", CURRENT_SKILL_ID)
+            # logger.warning("Id: %d - Accept", CURRENT_QUESTION_ID)
+        elif message.text.lower().strip() == 'нет':
+            logger.info("Забраковали навык с id: %d", CURRENT_SKILL_ID)
+            database.confirm_skill(id=CURRENT_SKILL_ID, confirm=False)
+
+        # Получаем новый вопрос
+        skill = database.get_skill_from_database()
+        if not skill:
+            logger.warning("Закончились вопросы")
+            await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
+            quit()
+        CURRENT_SKILL_ID = skill.iD
+        print(CURRENT_SKILL_ID)
+
+        # Показываем вопрос пользователю
+        await States.question.set() # Показываем следующий вопрос
+        _, remains = database.show_the_rest()
+        await message.answer(f"Оставить навык?\n- {skill.title}\nОсталось:{remains}", reply_markup=markup)
+
+
+# Функция для удаления сообщений
+async def delete_message(message: types.Message, sleep_time: int = 0):
+    await asyncio.sleep(sleep_time)
+    with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
+        await message.delete()
+
+
+# Обработка исключений
+@dp.message_handler(state=States.question)
+async def input_invalid(message: types.Message):
+    await States.question.set()
+    return await message.reply("Неправильный ответ. Используйте клавиатуру или введите ответ самостоятельно")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
