5cbadea936625de361e76a030c989a37a37a0efd
Rosya-edwica <rosya-8@yandex.ru>
2023-05-16 15:16:42 +0300

    связал ответы пользователя с редисом

--- leave-or-not/database.py
+++ leave-or-not/database.py
@@ -1,7 +1,8 @@
 import pymysql
 from pymysql.connections import Connection
-
+from redis import Redis
 from config import Skill, MYSQL
+from typing import Literal
 
 
 def connect_to_db() -> Connection:
@@ -19,47 +20,37 @@ def connect_to_db() -> Connection:
         exit(f"Error by connection: {ex}")
 
 
-def get_skill_from_database() -> Skill:
+def get_skill_from_database(db: Connection) -> Skill:
     """Возвращает самую первую пару навыков, у которой значение is_duplicate является None. 
     Таким образом, мы понимаем, что с этой парой еще не работали"""
     
-    connection = connect_to_db()
-    with connection.cursor() as cursor:
+    with db.cursor() as cursor:
         query_get_null_couple = f"SELECT id, name, is_displayed FROM {MYSQL.TABLE.value}  WHERE is_displayed IS NULL LIMIT 1"
 
         cursor.execute(query_get_null_couple)
         skill = cursor.fetchone()
-        connection.close()
         if skill:return Skill(*skill)
         else:return
 
-def confirm_skill(id: int, confirm: bool = True) -> None:
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            if confirm:
-                cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=1 WHERE id={id}")
-            else:
-                cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=0 WHERE id={id}")
-
-            connection.commit()
-    finally:
-        connection.close()
+def confirm_skill(db: Connection, id: int, confirm: bool = True) -> None:
+    with db.cursor() as cursor:
+        if confirm:
+            cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=1 WHERE id={id}")
+        else:
+            cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=0 WHERE id={id}")
+        db.commit()
 
 
-def refute_skill(id: int) -> None: # Опровергнуть сходство
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=null WHERE id={id}")
-            connection.commit()
 
-    finally:
-        connection.close()
+def refute_skill(db: Connection, id: int) -> None: # Опровергнуть сходство
+    with db.cursor() as cursor:
+        cursor.execute(f"UPDATE {MYSQL.TABLE.value} SET is_displayed=null WHERE id={id}")
+        db.commit()
 
-def get_previos_skill(current_id: int) -> Skill | None:
-    connection = connect_to_db()
-    with connection.cursor() as cursor:
+
+
+def get_previos_skill(db: Connection, current_id: int) -> Skill | None:
+    with db.cursor() as cursor:
         cursor.execute(f"""SELECT id, name, is_displayed FROM {MYSQL.TABLE.value} WHERE id = {current_id-1}""")
         skill = cursor.fetchone()
 
@@ -67,23 +58,22 @@ def get_previos_skill(current_id: int) -> Skill | None:
         except: res = None 
 
         if res is None: return
-        refute_skill(id=res.iD) # Делаем это для того, чтобы мы могли несколько раз подряд нажимать кнопку назад 
-        connection.close()
+        refute_skill(db, id=res.iD) # Делаем это для того, чтобы мы могли несколько раз подряд нажимать кнопку назад 
         return res
 
-def show_the_rest() -> tuple[int, int]:
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value}")
-            count_all_values = cursor.fetchone()
+def show_the_rest(db: Connection) -> tuple[int, int]:
+    with db.cursor() as cursor:
+        cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value}")
+        count_all_values = cursor.fetchone()
 
-            cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value} WHERE is_displayed IS NULL")
-            count_null_values = cursor.fetchone()
-            return count_all_values[0], count_null_values[0]
-    except BaseException as err:
-        exit(err)
-    finally:
-        connection.close()
+        cursor.execute(f"SELECT COUNT(*) FROM {MYSQL.TABLE.value} WHERE is_displayed IS NULL")
+        count_null_values = cursor.fetchone()
+        return count_all_values[0], count_null_values[0]
 
 
+def add_skill_to_redis(redisCli: Redis, skill: str, setName: Literal["minus_skill", "correct_skill"]):
+    redisCli.sadd(setName, skill)
+
+def refute_skill_from_redis(redisCli: Redis, skill: str):
+    redisCli.srem("minus_skill", skill)
+    redisCli.srem("correct_skill", skill)
--- leave-or-not/main.py
+++ leave-or-not/main.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.exceptions import MessageCantBeDeleted, MessageToDeleteNotFound
 from aiogram.dispatcher import FSMContext
 import asyncio
+from redis import Redis
 from loguru import logger
 
 import os
@@ -13,16 +14,25 @@ from config import States, TOKEN, add_user_action, create_notify, get_my_statist
 
 
 CURRENT_SKILL_ID = None
+CURRENT_SKILL = None
 
 bot = Bot(TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+db = None
+redisCli = None
 
 os.makedirs("logs/", exist_ok=True)
 logger.remove()
 logger.add("logs/info.log", format="{time} {level} {message}", level="INFO", rotation="10 MB", compression="zip", mode="w")
 
 
+def main():
+    global db, redisCli
+    db = database.connect_to_db()
+    redisCli = Redis("localhost", 6379)
+    create_notify()
+    executor.start_polling(dp, skip_updates=True)
 
 @dp.message_handler(commands='start')
 async def run_bot(message: types.Message):
@@ -50,21 +60,22 @@ async def start(message: types.Message):
     """Вспомогательный метод для запуска первого вопроса. Без этого метода, дублируются первые два вопросы и сбивается весь порядок ответов
     Поэтому был написан этот метод, как отправная точка и добавлена переменная CURRENT_QUESTION_ID"""
 
-    global CURRENT_SKILL_ID
+    global CURRENT_SKILL_ID, CURRENT_SKILL
     if message.text.lower() != "готов":
         asyncio.create_task(input_invalid(message))
         logger.info("Неправильный ответ на вопрос '%s'", message.text)
         return
 
-    skill = database.get_skill_from_database()
+    skill = database.get_skill_from_database(db)
     if not skill:
         logger.warning("Закончились вопросы")
         await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
         quit()
     CURRENT_SKILL_ID = skill.iD # Меняем значение нашей переменной, тем самым указывая корректный айди вопроса, который нужно обработать
+    CURRENT_SKILL = skill.title
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Нет', 'Да')
-    _, remains = database.show_the_rest()
+    _, remains = database.show_the_rest(db)
     await message.answer(f"Оставить навык?\n- {skill.title}\nОсталось:{remains}", reply_markup=markup)
     await States.question.set()
 
@@ -74,7 +85,7 @@ async def show_question(message: types.Message, state: FSMContext):
     """
     Основной метод, который будет обрабатывать ответы пользователя и изменять значения в БД
     """
-    global CURRENT_SKILL_ID
+    global CURRENT_SKILL_ID, CURRENT_SKILL
     if message.text.lower() not in {"да", "назад", "нет"}:
         asyncio.create_task(input_invalid(message))
         return
@@ -83,12 +94,14 @@ async def show_question(message: types.Message, state: FSMContext):
     markup.add('Нет', 'Назад', 'Да')
     if message.text.lower().strip() == 'назад':
         add_user_action(actionName="cancel", message=message)
-        previos_skill = database.get_previos_skill(CURRENT_SKILL_ID)
+        previos_skill = database.get_previos_skill(db, CURRENT_SKILL_ID)
         if previos_skill:
             await States.question.set()
-            _, remains = database.show_the_rest()
+            database.refute_skill_from_redis(redisCli, previos_skill.title)
+            _, remains = database.show_the_rest(db)
             await message.answer(f"Оставить навык?\n- {previos_skill.title}\nОсталось:{remains}", reply_markup=markup)
             CURRENT_SKILL_ID = previos_skill.iD
+            CURRENT_SKILL = previos_skill.title
 
         else:
             await message.answer("Ранее вы еще не отвечали на вопросы!")
@@ -96,26 +109,29 @@ async def show_question(message: types.Message, state: FSMContext):
         # Меняем значения в БД
         if message.text.lower().strip() == 'да':
             add_user_action(actionName="yes", message=message)
-            database.confirm_skill(id=CURRENT_SKILL_ID)
+            database.confirm_skill(db, id=CURRENT_SKILL_ID)
+            database.add_skill_to_redis(redisCli, CURRENT_SKILL, "correct_skill")
             logger.info("Прошел проверку навык с id: %d", CURRENT_SKILL_ID)
             # logger.warning("Id: %d - Accept", CURRENT_QUESTION_ID)
         elif message.text.lower().strip() == 'нет':
             add_user_action(actionName="no", message=message)
             logger.info("Забраковали навык с id: %d", CURRENT_SKILL_ID)
-            database.confirm_skill(id=CURRENT_SKILL_ID, confirm=False)
+            database.confirm_skill(db, id=CURRENT_SKILL_ID, confirm=False)
+            database.add_skill_to_redis(redisCli, CURRENT_SKILL, "minus_skill")
 
         # Получаем новый вопрос
-        skill = database.get_skill_from_database()
+        skill = database.get_skill_from_database(db)
         if not skill:
             logger.warning("Закончились вопросы")
             await message.answer("Все вопросы закончились! Спасибо", reply_markup=types.ReplyKeyboardRemove())
             quit()
         CURRENT_SKILL_ID = skill.iD
+        CURRENT_SKILL = skill.title
         print(CURRENT_SKILL_ID)
 
         # Показываем вопрос пользователю
         await States.question.set() # Показываем следующий вопрос
-        _, remains = database.show_the_rest()
+        _, remains = database.show_the_rest(db)
         await message.answer(f"Оставить навык?\n- {skill.title}\nОсталось:{remains}", reply_markup=markup)
 
 
@@ -140,8 +156,5 @@ async def input_invalid(message: types.Message):
     return await message.reply("Неправильный ответ. Используйте клавиатуру или введите ответ самостоятельно")
 
 
-
-
 if __name__ == "__main__":
-    create_notify()
-    executor.start_polling(dp, skip_updates=True)
+    main()
